5868fa01a1281580cdfd53b0a9c8c0069b62205f
jiawei <jiaweiwu@brandeis.edu>
2023-10-21 13:55:55 -0400

    index.ejs, render programs and membership

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,23 +1,30 @@
 // Response to a request coming from the packages / views
-// Requiring this at the index.js main application file 
+// Requiring this at the index.js main application file
 exports.respondHome = (req, res) => {
-    res.render("index");
-
-    };
+  res.render("index");
+};
 exports.aboutUs = (req, res) => {
-    res.render("about");
-    };
+  res.render("about");
+};
 exports.facilitiesItem = (req, res) => {
-    res.render("facilities");
-    };
-    
-    exports.event = (req, res) => {
-        res.render("event");
-        };
-    exports.contact = (req, res) => {
-        res.render("contact");
-        };
-    exports.postedContact = (req, res) => {
-        const username = req.body.name; // This corresponds to the "name" field in your form
-    res.render('welcome', { username: username });
-    };
+  res.render("facilities");
+};
+
+exports.event = (req, res) => {
+  res.render("event");
+};
+exports.contact = (req, res) => {
+  res.render("contact");
+};
+exports.postedContact = (req, res) => {
+  const username = req.body.name; // This corresponds to the "name" field in your form
+  res.render("welcome", { username: username });
+};
+
+exports.membership = (req, res) => {
+  res.render("membership");
+};
+
+exports.programs = (req, res) => {
+  res.render("programs");
+};
--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 const express = require("express");
-const homeController = require('./controllers/homeController');
+const homeController = require("./controllers/homeController");
 const layouts = require("express-ejs-layouts");
 
 const app = express();
-// Start the view engine and require the layouts 
+// Start the view engine and require the layouts
 app.set("view engine", "ejs");
 app.use(layouts);
-// utilize the static files to access the food.jpg images 
+// utilize the static files to access the food.jpg images
 app.use(express.static("public"));
 
 app.set("port", process.env.PORT || 8080);
 // env = environmnet, can be assigned by any values, or default values
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.get("/", homeController.respondHome);
 // have one route, a request coming from the HTTP
 app.get("/", (req, res) => {
-res.send("Welcome to Brandeis Sports Complex");
+  res.send("Welcome to Brandeis Sports Complex");
 });
 // adding three more routes that using callback functions
 app.get("/about", homeController.aboutUs);
@@ -24,10 +24,10 @@ app.get("/facilities", homeController.facilitiesItem);
 app.get("/event", homeController.event);
 app.get("/contact", homeController.contact);
 app.post("/contact", homeController.postedContact);
-
-
+app.get("/membership", homeController.membership);
+app.get("/programs", homeController.programs);
 
 // Start the server
 app.listen(app.get("port"), () => {
-console.log(`Server running at http://localhost:${app.get("port")}`);
+  console.log(`Server running at http://localhost:${app.get("port")}`);
 });
